704d26b6df0d5f6db3de869f13348b832cc653a9
Nick Brown <njbrown09@gmail.com>
2023-04-17 13:21:55 -0400

    Works!

--- src/events/create_event.php
+++ src/events/create_event.php
@@ -1,6 +1,6 @@
 <?php
-include_once('core/header.php');
-require_once('core/db.php');
+include_once('../core/header.php');
+require_once('../core/db.php');
 
 $error = null;
 
@@ -25,7 +25,7 @@ if (isset($_POST['submit'])) {
 
     try {
         // Insert the RSO
-        $stmt = $dbConn->prepare("INSERT INTO EVENTS (Name, Category, Description, Time, Date, LocationID, ConatactPhone, ContactEmail) VALUES (:eventName, :category, :description, :time, :date, :locationId, :,contactPhone, :contactEmail)");
+        $stmt = $dbConn->prepare("INSERT INTO EVENTS (Name, Category, Description, Time, Date, LocationID, ContactPhone, ContactEmail) VALUES (:eventName, :category, :description, :time, :date, :locationId, :contactPhone, :contactEmail)");
         $stmt->bindParam(':eventName', $eventName);
         $stmt->bindParam(':category', $category);
         $stmt->bindParam(':description', $description);
@@ -35,7 +35,7 @@ if (isset($_POST['submit'])) {
         $stmt->bindParam(':contactPhone', $contactPhone);
         $stmt->bindParam(':contactEmail', $contactEmail);
         $stmt->execute();
-      
+
 
     } catch (PDOException $e) {
         $error = "Error creating RSO: " . $e->getMessage();
@@ -57,7 +57,7 @@ if (isset($_POST['submit'])) {
     <div class="card text-center w-25">
         <div class="container">
             <h1>Host Event</h1>
-            <form class="text-start" action="/organizations/create_rso.php" method="post">
+            <form class="text-start" action="/events/create_event.php" method="post">
                 <div class="mb-3">
                     <label for="rsoName" class="form-label">Event Name</label>
                     <input type="text" class="form-control" id="eventName" name="eventName" required>
@@ -72,28 +72,22 @@ if (isset($_POST['submit'])) {
                 </div>
                 <div class="form-group mb-2">
                     <label for="inputEventDescription" required="required">Description</label>
-                    <input style="height:180px; padding-bottom:150px" type="text" name="description" class="form-control" id="description" aria-describedby="emailHelp" placeholder="Enter Description..." maxlength="500">
+                    <input style="height:180px; padding-bottom:150px" type="text" name="description"
+                           class="form-control" id="description" aria-describedby="emailHelp"
+                           placeholder="Enter Description..." maxlength="500">
                 </div>
-                <div class="container px-1 px-sm-5 mx-auto mt-2">
-                    <form autocomplete="off">
-                        <div class="flex-row d-flex justify-content-center">
-                            <div class="col-lg-6 col-11 px-1 r">
-                                <div class="input-group input-daterange"> 
-                                    <label class="ml-3 form-control-placeholder mr-3" id="date" for="start">Date:&nbsp;</label>                                     
-                                    <input type="date" id="date" class="form-control text-left mr-2"> 
-                                    <span class="fa fa-calendar" id="fa-1"></span> 
-                                    <span class="fa fa-calendar" id="fa-2"></span> 
-                                </div>
-                            </div>
-                        </div>
-                    </form>
+
+
+                <div class="form-group mb-2">
+                    <label class="ml-3 form-control-placeholder mr-3" id="date" for="start">Date:&nbsp;</label>
+                    <input type="date" id="date" name ="date" class="form-control text-left mr-2">
                 </div>
 
                 <div class="container px-1 px-sm-5 mx-auto mt-2">
                     <div class="d-flex justify-content-center">
-                    <label for="appt">Start Time:&nbsp;</label>
-                    <input type="time" id="time" name="time"
-                           min="06:00" max="18:00" required>
+                        <label for="appt">Start Time:&nbsp;</label>
+                        <input type="time" id="time" name="time"
+                               min="06:00" max="18:00" required>
                     </div>
                 </div>
                 <div class="mt-2 d-flex justify-content-center">
@@ -108,13 +102,14 @@ if (isset($_POST['submit'])) {
                     <label for="rsoName" class="form-label">Email Address</label>
                     <input type="text" class="form-control" id="contactEmail" name="contactEmail" required>
                 </div>
-                <button type="submit" id="submit" name="submit" class="btn btn-primary text-center">Create Event</button>
+                <button type="submit" id="submit" name="submit" class="btn btn-primary text-center">Create Event
+                </button>
             </form>
         </div>
 
         <script>
-            $(document).ready(function() {
-                $("#addMoreMembers").click(function() {
+            $(document).ready(function () {
+                $("#addMoreMembers").click(function () {
                     $('<input type="email" class="form-control mb-2 memberEmails" id="memberEmails[]" name="memberEmails[]" placeholder="Email address">').appendTo("#memberEmailsContainer");
                 });
             });
