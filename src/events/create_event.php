96ee83b01c28b278ed7c462d8e15c9efb493b76c
jl01128 <jackie01128@gmail.com>
2023-04-17 14:00:54 -0400

    createEvents

--- src/events/create_event.php
+++ src/events/create_event.php
@@ -14,7 +14,10 @@ if (isset($_POST['submit'])) {
     $locationId = ($_POST['locationId']);
     $contactPhone = ($_POST['contactPhone']);
     $contactEmail = ($_POST['contactEmail']);
-
+    $eventType = ($_POST['eventType']);
+    $rsoID = ($_POST['rsoID']);
+    $universityID = ($_POST['universityID']);
+    $approved = ($_POST['approved']);
 
     // Get the current user's ID as the admin
     $adminID = $_SESSION["user_id"];
@@ -22,7 +25,7 @@ if (isset($_POST['submit'])) {
 
     try {
         // Insert the RSO
-        $stmt = $dbConn->prepare("INSERT INTO EVENTS (Name, Category, Description, Time, Date, LocationID, ContactPhone, ContactEmail) VALUES (:eventName, :category, :description, :time, :date, :locationId, :contactPhone, :contactEmail)");
+        $stmt = $dbConn->prepare("INSERT INTO EVENTS (Name, Category, Description, Time, Date, LocationID, ContactPhone, ContactEmail, EventType, RSOID, UniversityID, APPROVED) VALUES (:eventName, :category, :description, :time, :date, :locationId, :contactPhone, :contactEmail, :eventType, :rsoID, :universityID, :approved)");
         $stmt->bindParam(':eventName', $eventName);
         $stmt->bindParam(':category', $category);
         $stmt->bindParam(':description', $description);
@@ -31,8 +34,12 @@ if (isset($_POST['submit'])) {
         $stmt->bindParam(':locationId', $locationId);
         $stmt->bindParam(':contactPhone', $contactPhone);
         $stmt->bindParam(':contactEmail', $contactEmail);
+        $stmt->bindParam(':eventType', $eventType);
+        $stmt->bindParam(':rsoID', $rsoID);
+        $stmt->bindParam(':universityID', $universityID);
+        $stmt->bindParam(':approved', $approved);
+  
         $stmt->execute();
-
 
     } catch (PDOException $e) {
         $error = "Error creating RSO: " . $e->getMessage();
@@ -48,7 +55,7 @@ if (isset($_POST['submit'])) {
     </div>
 <?php endif; ?>
 
-<div class="h-100 d-flex align-items-center justify-content-center">
+<div class="h-60 d-flex align-items-center justify-content-center">
 
     <!--Create RSO card.-->
     <div class="card text-center w-25">
@@ -60,13 +67,10 @@ if (isset($_POST['submit'])) {
                     <input type="text" class="form-control" id="eventName" name="eventName" required>
                 </div>
                 <div class="mb-3">
-                    <label for="universityID" class="form-label">Event Category</label>
-                    <select class="form-control" id="category" name="category" required="required">
-                        <option>Public</option>
-                        <option>Private</option>
-                        <option>RSO</option>
-                    </select>
+                    <label for="rsoName" class="form-label">Category</label>
+                    <input type="text" class="form-control" id="category" name="category" required>
                 </div>
+                
                 <div class="form-group mb-2">
                     <label for="inputEventDescription" required="required">Description</label>
                     <input style="height:180px; padding-bottom:150px" type="text" name="description"
@@ -92,12 +96,32 @@ if (isset($_POST['submit'])) {
                     <input class="col-2" type="numver" name="locationId" id="locationId" maxlength="50">
                 </div>
                 <div class="mb-3">
-                    <label for="universityID" class="form-label">Phone</label>
+                    <label for="phone" class="form-label">Phone</label>
                     <input type="number" class="form-control" id="contactPhone" name="contactPhone" required>
                 </div>
                 <div class="mb-3">
                     <label for="rsoName" class="form-label">Email Address</label>
                     <input type="text" class="form-control" id="contactEmail" name="contactEmail" required>
+                </div>
+                <div class="mb-3">
+                    <label for="eventType" class="form-label">Event type</label>
+                    <select class="form-control" id="eventType" name="eventType" required="required">
+                        <option>Public</option>
+                        <option>Private</option>
+                        <option>RSO</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="rsoID" class="form-label">RSOID</label>
+                    <input type="number" class="form-control" id="rsoID" name="rsoID" required>
+                </div>
+                <div class="mb-3">
+                    <label for="universityID" class="form-label">UniversityID</label>
+                    <input type="number" class="form-control" id="universityID" name="universityID" required>
+                </div>
+                <div class="mb-3">
+                    <label for="approved" class="form-label">Super Admin Approval</label>
+                    <input type="number" class="form-control" id="approved" name="approved" required>
                 </div>
                 <button type="submit" id="submit" name="submit" class="btn btn-primary text-center">Create Event
                 </button>
