2dbf51cd4efe46d17df71ecdc433897f7f52e591
Nick Brown <njbrown09@gmail.com>
2023-04-17 13:37:19 -0400

    Update index.php

--- src/events/index.php
+++ src/events/index.php
@@ -1,41 +1,44 @@
 <?php
 include_once('../core/header.php');
+
+// Get the user ID and university ID from the session
+$user_id = $_SESSION["user_id"];
+$university_id = $_SESSION["user_universityid"];
+
+// Fetch RSOs for the user's university
+$stmt = $dbConn->prepare('SELECT * FROM events WHERE UniversityID = :university_id');
+$stmt->execute(['university_id' => $university_id]);
+$events = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
-
-<?php
-
-?>
-
-<div class="h-100 d-flex align-items-center justify-content-center container">
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-
-        <?php for($i = 0; $i < 8; $i++): ?>
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Event Name</h5>
-                    <p class="card-text">Event description.</p>
+    <div class="container">
+        <div class="row">
+            <?php foreach ($events as $event) : ?>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?=$event["Name"];?></h5>
+                            <p class="card-text"><?=$event["Description"];?></p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><?=$event["Category"];?></li>
+                            <li class="list-group-item"><?=$event["Date"];?></li>
+                            <li class="list-group-item"><?=$event["Time"];?></li>
+                        </ul>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><?=$event["ContactEmail"];?></li>
+                            <li class="list-group-item"><?=$event["ContactPhone"];?></li>
+                        </ul>
+                        <div class="card-body">
+                            <a href="#" class="card-link">View Event</a>
+                            <a href="#" class="card-link">Edit event</a>
+                        </div>
+                    </div>
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Event Category</li>
-                    <li class="list-group-item">Event Date</li>
-                    <li class="list-group-item">Event Time</li>
-                </ul>
-                <div class="card-body">
-                    <a href="#" class="card-link">View Event</a>
-                    <a href="#" class="card-link">Edit event</a>
-                </div>
-            </div>
+            <?php endforeach; ?>
         </div>
-        <?php endfor; ?>
     </div>
-</div>
-
 
 <?php
 include_once('../core/footer.php');
 ?>
-
-
